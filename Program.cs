db7eb45c64468708fc7290ff203f514a4834fb45
gharjare <gharjarechetan@gmail.com>
2022-12-22 13:35:15 +0530

    Add program file

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MoodAnalyser1
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Sad or Happy");
+            
+            string input = Console.ReadLine();
+
+            MoodAnalyse analyse = new MoodAnalyse(input);
+            Console.WriteLine(analyse.Analyse);
+            Console.WriteLine(analyse.Analyse1);
+            Console.WriteLine(analyse.Analyse2);
+
+            
+           
+             
+        }
+    }
+}
