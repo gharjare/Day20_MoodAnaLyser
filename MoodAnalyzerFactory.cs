338be6a01095e7af4f4e704856299c8dba36953a
gharjare <gharjarechetan@gmail.com>
2022-12-22 14:39:30 +0530

    create moodanalyzer factory

---
+++ MoodAnalyzerFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser1
+{
+    public class MoodAnalyzerFactory
+    {
+        public static object CreateMoodAnalyseUsingParameterizedConstrucor(string className, string constructorName,string input)
+        {
+
+            Type type = typeof(MoodAnalyse);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { "Happy" });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructure is not found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+            }
+        }
+    }
+}
